e5df8011b49811e30b19810cf232b05f97bd6b46
Mark Ross <mark.ross@atos.net>
2022-01-07 09:57:08 +0000

    initial commit for testing

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # aws-github-oidc
 Repo using OIDC to authenticate to AWS from GitHub Actions
+
+Instructions: -
+  - Authenticate to AWS account you want to depploy to using youre typical method (temp creds, access key / secret access key etc)
+  - Run the folliwing commands
+    - terraform init
+    - terraform apply
+      - enter the region, github org and github repo you're deploying from
+  - Add the following GitHub Secrets to your repository (within GitHub repo go to settings, secrets and create new secrets)
+    - AWS_REGION - set the region you want to deploy to
+    - AWS_ACCOUNT_ID - set as the AWS account number you've deployed the Terraform code to
+  - Run the GitHub action that can now authenticate using OIDC
+    - Navigate to GitHub actions
---
+++ data.tf
@@ -0,0 +1,25 @@
+#Data object to create pipeline assume role policy
+data "aws_iam_policy_document" "pipeline_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.organization}/${var.repository_name}:*"]
+    }
+  }
+}
+#Data object to create piepline policy
+#tfsec:ignore:AWS099 Pipeline deliberately given full access to be able to provision all resources
+data "aws_iam_policy_document" "pipeline" {
+  statement {
+    actions = [
+      "*",
+    ]
+    resources = ["*"]
+  }
+}
